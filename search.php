1076a49bc4bf11611a6d100f065e4b9ea3b1111c
1upnate <nzspiegel@e-dev.us>
2016-04-17 08:56:10 -0400

    adjusted css on search results page to match the rest.

--- search.php
+++ search.php
@@ -1,15 +1,20 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <div id="container" style="width:100%;">
 
     <div id="left" style="float:left; width:50%;">
         <p>Results matching by Subject:</p>
         <br><br>
-        <?php include 'subjectsearch.php'; ?>
+        <p><?php include 'subjectsearch.php'; ?>
     </div>
 
 
     <div id="right" style="float:right; width:50%;">
         <p>Results matching by content:</p>
         <br><br>
-        <?php include 'extract_search.php'; ?>
+        <p><?php include 'extract_search.php'; ?>
     </div>
 </div>
+</html>
